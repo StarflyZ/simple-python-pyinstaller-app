16c2219472234716c55f50cad637b804b498e014
Steven Nataniel <stevnathar@gmail.com>
2025-02-21 13:11:43 +0700

    Deploy Stage

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -3,26 +3,17 @@
 echo 'Starting Python application deployment...'
 
 set -x
-# Membuat virtual environment jika belum ada
 if [ ! -d "venv" ]; then
     python -m venv venv
 fi
-
-# Aktifkan virtual environment
 . venv/bin/activate
-
-# Install dependencies jika ada
 pip install --upgrade pip
 pip install -r requirements.txt
 set +x
 
 echo 'Running Python application...'
-
-# Jalankan aplikasi dengan output ke log & tampilkan di Jenkins
 python app.py 2>&1 | tee app.log &
-
-# Simpan PID aplikasi
 echo $! > .pidfile
 
 echo 'Python application is running! Logs:'
-tail -f app.log &  # Menampilkan log real-time di Jenkins
+tail -f app.log & 
